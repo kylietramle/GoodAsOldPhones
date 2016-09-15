5680909f240420a454934ed13fc7169fe5fde150
Kylie Le <kylie.tram.le@gmail.com>
2016-09-15 11:02:29 -0700

    dynamic data in tableview

--- ProductsTableViewController.swift
+++ ProductsTableViewController.swift
@@ -9,16 +9,30 @@
 import UIKit
 
 class ProductsTableViewController: UITableViewController {
+    
+    var productNames: [String]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        productNames = ["1907 Wall Set", "1921 Dial Phone", "1937 Desk Set", "1984 Motorola Portable"]
+    }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        if let pNames = productNames { // if let returns an Int, not optional Int
+        return pNames.count
+        }
+        return 0
     }
     
-   
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("ProductCell", forIndexPath: indexPath)
         
-        cell.textLabel?.text = "Hello Willy"
+        let productName = productNames?[indexPath.row]
+        
+        if let pName = productName {
+            cell.textLabel?.text = pName
+        }
         cell.imageView?.image = UIImage(named: "image-cell1")
         
         return cell
