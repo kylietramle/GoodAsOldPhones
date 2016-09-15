39f558a0ea400ca6b47db2c13172dfa7221e789d
Kylie Le <kylie.tram.le@gmail.com>
2016-09-15 12:59:44 -0700

    add 'product' class to project

---
+++ GoodAsOldPhones/Product.swift
@@ -0,0 +1,15 @@
+//
+//  Product.swift
+//  GoodAsOldPhones
+//
+//  Created by Kylie Tram Le on 9/15/16.
+//  Copyright © 2016 Kylie Tram Le. All rights reserved.
+//
+
+import Foundation
+
+class Product {
+    var name: String?
+    var productImage: String?
+    var cellImage: String?
+}
--- ProductsTableViewController.swift
+++ ProductsTableViewController.swift
@@ -10,17 +10,38 @@ import UIKit
 
 class ProductsTableViewController: UITableViewController {
     
-    var productNames: [String]?
+    var products: [Product]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        productNames = ["1907 Wall Set", "1921 Dial Phone", "1937 Desk Set", "1984 Motorola Portable"]
+        let product1 = Product()
+        let product2 = Product()
+        let product3 = Product()
+        let product4 = Product()
+        
+        product1.name = "1907 Wall Set"
+        product1.productImage = "phone-fullscreen1"
+        product1.cellImage = "image-cell1"
+        
+        product2.name = "1921 Dial Phone"
+        product2.productImage = "phone-fullscreen2"
+        product2.cellImage = "image-cell2"
+        
+        product3.name = "1937 Desk Set"
+        product3.productImage = "phone-fullscreen3"
+        product3.cellImage = "image-cell3"
+        
+        product4.name = "1984 Motorola Portable"
+        product4.productImage = "phone-fullscreen4"
+        product4.cellImage = "image-cell4"
+        
+        products = [product1, product2, product3, product4]
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let pNames = productNames { // if let returns an Int, not optional Int
-            return pNames.count
+        if let p = products { // if let returns an Int, not optional Int
+            return p.count
         }
         return 0
     }
@@ -28,12 +49,14 @@ class ProductsTableViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("ProductCell", forIndexPath: indexPath)
         
-        let productName = productNames?[indexPath.row]
+        let product = products?[indexPath.row]
         
-        if let pName = productName {
-            cell.textLabel?.text = pName
+        if let p = product {
+            cell.textLabel?.text = p.name
+            if let i = p.cellImage {
+                cell.imageView?.image = UIImage(named: i)
+            }
         }
-        cell.imageView?.image = UIImage(named: "image-cell1")
         
         return cell
     }
@@ -47,7 +70,7 @@ class ProductsTableViewController: UITableViewController {
                 let indexPath = tableView.indexPath(for: cell) else {
                     return
             }
-            productVC?.productName = productNames?[indexPath.row]
+            productVC?.product = products?[indexPath.row]
                     
         }
     }
